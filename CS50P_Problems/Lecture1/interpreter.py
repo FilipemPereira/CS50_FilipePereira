e277c83a33243d6e4c11c3cb23f286668b35ed5c
FilipemPereira <fmmpereira2003@gmail.com>
2023-05-23 15:30:44 +0100

    day2-2

---
+++ CS50P_Problems/Lecture1/interpreter.py
@@ -0,0 +1,23 @@
+def mathInterpreter(input) -> str:
+    x, operator, y = input.split(" ")
+    n1 = int(x)
+    n2 = int(y)
+    match operator:
+        case "+":
+            result = n1 + n2
+        case "-":
+            result = n1 - n2
+        case "*":
+            result = n1 * n2
+        case "/":
+            result = float(n1) / n2
+        case _:
+            result = "ERROR!"
+
+    return result
+
+def main():
+    line = input("Expression: ")
+    print(mathInterpreter(line))
+
+main()
