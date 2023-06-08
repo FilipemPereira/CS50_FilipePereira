970add365d455f01bb17d72fa68fc2ba3d6bdbce
FilipemPereira <fmmpereira2003@gmail.com>
2023-06-08 16:08:37 +0100

    day4-1

--- CS50P_Problems/Lecture0/einstein.py
+++ CS50P_Problems/Lecture0/einstein.py
@@ -1,3 +1,9 @@
+"""
+Calcule E = mc^2!
+In a file called einstein.py, implement a program in Python that prompts the user for mass as an integer (in kilograms) and then outputs the equivalent number
+of Joules as an integer. Assume that the user will input an integer.
+"""
+
 def calculation(mass):
     c: int = 300000000
     return mass * c**2
--- CS50P_Problems/Lecture0/faces.py
+++ CS50P_Problems/Lecture0/faces.py
@@ -1,3 +1,9 @@
+""""
+In a file called faces.py, implement a function called convert that accepts a str as input and returns that same input with any :) converted to 🙂 
+(otherwise known as a slightly smiling face) and any :( converted to 🙁 (otherwise known as a slightly frowning face). All other text should be returned 
+unchanged.
+"""
+
 def replace(phrase):
     # replace - replace a substring for another in the original str object
     phrase = phrase.replace(":)", "🙂")
