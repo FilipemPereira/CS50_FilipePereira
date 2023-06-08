bd6038503c593f3d437661130227e8d56a370263
FilipemPereira <fmmpereira2003@gmail.com>
2023-06-08 18:06:58 +0100

    day4-2

--- CS50P_Problems/Lecture2/coke.py
+++ CS50P_Problems/Lecture2/coke.py
@@ -1,17 +1,24 @@
+"""
+Suppose that a machine sells bottles of Coca-Cola (Coke) for 50 cents and only accepts coins in these denominations: 25 cents, 10 cents, and 5 cents.
+
+In a file called coke.py, implement a program that prompts the user to insert a coin, one at a time, each time informing the user of the amount due. 
+Once the user has inputted at least 50 cents, output how many cents in change the user is owed. Assume that the user will only input integers, and ignore 
+any integer that isn't an accepted denomination
+"""
 
 def main():
-    sum = int()
+    sum = 0
     print("Amount Due: 50")
     while sum < 50:
         coin = int(input("Insert coin: "))
-        #if (coin != 25) or (coin != 10) or (coin != 5):
-        #    break
+        while coin not in [5, 10, 25]:
+            coin = int(input("Insert coin: "))
         sum += coin
-        remainer = 50 - coin
-        if remainer == 0:
+        remaining = 50 - coin
+        if remaining == 0:
             print("Change Owed: 0")
         else:
-            print(f"Amount Due: {50 - sum}")
+            print(f"Amount Due: {remaining}")
       
 
 main()
--- CS50P_Problems/Lecture2/twttr.py
+++ CS50P_Problems/Lecture2/twttr.py
@@ -1,3 +1,10 @@
+"""
+When texting or tweeting, it's not uncommon to shorten words to save time or space, as by omitting vowels, much like Twitter was originally called twttr. 
+In a file called twttr.py, implement a program that prompts the user for a str of text and then outputs that same text but with all vowels (A, E, I, O, and
+U) omitted, whether inputted in uppercase or lowercase.
+"""
+
+# verify if a letter is vowel
 def is_vowel(letter):
     if letter.lower() in ['a', 'e', 'i', 'o', 'u']:
         return True
