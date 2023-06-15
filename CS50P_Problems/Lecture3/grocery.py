22b042e7ae35bb76d1f55be12010a017d9bce369
FilipemPereira <fmmpereira2003@gmail.com>
2023-06-15 20:52:40 +0100

    day5-1

--- CS50P_Problems/Lecture3/grocery.py
+++ CS50P_Problems/Lecture3/grocery.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 """
-implement a program that prompts the user for items, one per line, until the user inputs control-d (which is a common way of ending one’s input to a 
+implement a program that prompts the user for items, one per line, until the user inputs control-d (which is a common way of ending one's input to a 
 program). Then output the user's grocery list in all uppercase, sorted alphabetically by item, prefixing each line with the number of times the user 
 inputted that item. No need to pluralize the items. Treat the user's input case-insensitively.
 """
